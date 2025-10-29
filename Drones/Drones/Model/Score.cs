e1f5eb6c06e3c593061feba2896d16571e2855cf
killian923 <144005114+killian923@users.noreply.github.com>
2025-10-29 14:32:17 +0100

    Feat(Score) : début du code du score

---
+++ Drones/Drones/Model/Score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Model
+{
+    public partial class Score
+    {
+        private int score;
+
+        public int _score { get => score; set => score = value; }
+
+        public Score()
+        {
+            _score = 0;
+        }
+
+
+        public void update()
+        {
+            _score += 1;
+        }
+    }
+}
---
+++ Drones/Drones/View/Score.cs
@@ -0,0 +1,27 @@
+﻿using Drones.Helpers;
+using Drones.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Drones.View
+{
+    public partial class Score
+    {
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            //drawingSpace.Graphics.DrawRectangle();
+            //drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush);
+        }
+
+        // De manière textuelle
+        public override string ToString()
+        {
+            return $"({((int)((double)_score)).ToString()}%)";
+        }
+
+    }
+}
